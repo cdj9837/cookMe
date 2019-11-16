fe995f479bbd3f7c61ab3537c5175cc1ee76cb9e
mapumarshmallow <mapu.park@gmail.com>
2019-11-16 09:19:27 -0600

    ForgotPassword

--- app/src/main/java/com/example/cookme/Login.java
+++ app/src/main/java/com/example/cookme/Login.java
@@ -22,7 +22,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     Button loginBtn;
     EditText etEmail, etPassword;
-    TextView tvregisterLink;
+    TextView tvregisterLink, tvForgotPwLink;
 
     ProgressDialog progressDialog;
 
@@ -37,9 +37,11 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         etPassword = (EditText) findViewById(R.id.etPassword);
         loginBtn = (Button) findViewById(R.id.LoginBtn);
         tvregisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+        tvForgotPwLink = (TextView) findViewById(R.id.tvForgotPwLink);
 
         loginBtn.setOnClickListener(this);
         tvregisterLink.setOnClickListener(this);
+        tvForgotPwLink.setOnClickListener(this);
 
         progressDialog = new ProgressDialog(this);
 
@@ -72,6 +74,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 else{
                     Toast.makeText(Login.this, task.getException().getMessage(),
                             Toast.LENGTH_SHORT).show();
+                    progressDialog.hide();
                 }
             }
         });
@@ -82,11 +85,14 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         switch (view.getId()){
             case R.id.LoginBtn:
                 loginUser();
-//                startActivity(new Intent(this, Register.class));
-                 break;
+                break;
             case R.id.tvRegisterLink:
                 Intent i = new Intent(this, Register.class);
                 startActivity(i);
+                break;
+            case R.id.tvForgotPwLink:
+                Intent g = new Intent(this, ForgotPassword.class);
+                startActivity(g);
                 break;
         }
     }
