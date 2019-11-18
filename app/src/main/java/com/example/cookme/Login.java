b32de1ac405dcf9b0622ef553559eff542e7ea7e
Carolyn Johnson <carolyn.johnson2@mavs.uta.edu>
2019-11-17 20:09:47 -0600

    Adding list view

--- app/src/main/java/com/example/cookme/Login.java
+++ app/src/main/java/com/example/cookme/Login.java
@@ -17,6 +17,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
@@ -96,4 +97,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 break;
         }
     }
+
+
+
 }
